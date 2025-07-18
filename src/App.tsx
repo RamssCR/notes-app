675469e0552f42ea0746d00a3c2f25f370ba290b
RamssCR <ramsesgpm@gmail.com>
2025-07-17 21:43:59 -0500

    feat: agregada ruta con parametro opcional

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Notes } from '@views/Notes'
 import { useFontEffect } from '@hooks/useFontEffect'
 import { useThemeEffect } from '@hooks/useThemeEffect'
-import { CreateNote } from '@components/editor/CreateNote'
 
 /**
  * It's the main application component that sets up the routing for the app.
@@ -15,9 +14,8 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Notes />} />
+        <Route path="/:id?" element={<Notes />} />
       </Routes>
-      <CreateNote active={true} />
     </BrowserRouter>
   );
 }
