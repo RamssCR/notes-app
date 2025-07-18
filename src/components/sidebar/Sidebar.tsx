86adac36e24ebed3dc2758d956a8973cf5bd4c4f
RamssCR <ramsesgpm@gmail.com>
2025-07-17 21:48:38 -0500

    feat: added real notes on the sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,13 +1,21 @@
-import { CreateNote } from "./CreateNote"
+import { CreateNote } from "@components/editor/CreateNote"
+import { CreateNote as CreateNoteButton } from "./CreateNote"
 import { Note } from "./Note"
 import { Text } from "@components/ui/Text"
 import { Title } from "@components/ui/Title"
-import notes from '@data/notes.json'
+import { useState } from "react"
+import { noteStore } from "@stores/noteStore"
+import { useParams } from "react-router-dom"
 
 /**
  * Sidebar component for the notes application.
  */
 export const Sidebar = () => {
+  const { id } = useParams<{ id: string }>()
+  const { notes } = noteStore()
+  const [active, setActive] = useState(false)
+  const toggle = () => setActive(!active)
+
   return (
     <aside className="hidden bg-muted w-full h-[100svh] lg:col-span-1 xl:col-span-1 lg:flex flex-col items-start border-r border-border">
       <section className="flex flex-col items-start gap-4 py-4 px-6">
@@ -22,17 +30,29 @@ export const Sidebar = () => {
             Your notes
           </Text>
           <article className="w-full flex flex-col items-start gap-0.5 overflow-y-auto">
-            {notes.map(note => (
+            {notes
+              .slice()
+              .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+              .map(note => (
               <Note
                 key={note.id}
                 id={note.id}
                 title={note.title}
+                active={id === note.id}
               />
             ))}
           </article>
         </section>
       </section>
-      <CreateNote className="mt-auto" />
+      <CreateNoteButton 
+        className="mt-auto" 
+        onClick={toggle}
+        aria-label="Create Note"
+      />
+      <CreateNote
+        active={active}
+        onClose={toggle}
+      />
     </aside>
   )
 }
