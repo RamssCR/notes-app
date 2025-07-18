44a36fa3ec8867e6eb1bb11a99bce6bb33216790
RamssCR <ramsesgpm@gmail.com>
2025-07-17 21:47:22 -0500

    feat: added active validation

--- src/components/sidebar/Note.tsx
+++ src/components/sidebar/Note.tsx
@@ -1,22 +1,28 @@
 import type { ComponentProps } from "react"
 import type { NoteProps } from "@@types/note"
 import { Link } from "@components/ui/Link"
+import { classMerger } from "@utils/classMerger"
 
 type NoteComponentProps = 
   Pick<NoteProps, 'id' | 'title'> & 
-  Omit<ComponentProps<typeof Link>, 'to' | 'title' | 'id'>
+  Omit<ComponentProps<typeof Link>, 'to' | 'title' | 'id'> & {
+    active?: boolean
+  }
 
 /**
  * Note component for displaying a single note in the sidebar.
  * This component uses the Link component for navigation.
  * It displays the note title and provides a link to the note's detail page.
  */
-export const Note = ({ id, title, ...props }: NoteComponentProps) => {
+export const Note = ({ id, title, active = false, ...props }: NoteComponentProps) => {
   return (
     <Link
-      to={`/notes/${id}`}
+      to={`/${id}`}
       variant="sidebar"
-      className="text-sm line-clamp-1"
+      className={classMerger(
+        'text-sm line-clamp-1',
+        active && 'bg-slate-200 dark:bg-[#2c2b2b]',
+      )}
       title={title}
       {...props}
     >
